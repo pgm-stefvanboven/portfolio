e53dd56fda201719e7bad5f7d830c3af15d5c5cf
Stef Van Boven <stefvboven2020@outlook.com>
2023-05-11 23:32:55 +0200

    added some chanses form the given feedback

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -1,6 +1,6 @@
-const introArray = ["Met trots presenteer ik hierbij mijn persoonlijke portfolio website, op deze website vindt u informatie over mezelf, mijn projecten en een contactformulier indien u mij een vraag wil stellen, of een sugestie wilt doenn aarzel niet om contact met me op te nemen."];
+const introArray = ["Met trots presenteer ik hierbij mijn persoonlijke portfolio website op deze website vindt u informatie over mezelf."];
 let currentPosition = 0;
-const totalSpeed = 100;
+const totalSpeed = 10;
 
 const typewriterintro = () => {
   document.querySelector("#intro").innerHTML = introArray[0].substring(0, currentPosition) + "<span></span>";
