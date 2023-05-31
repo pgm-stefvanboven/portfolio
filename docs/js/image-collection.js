ff766e4dbcd60795c9a1834886752b67fee05fdb
Stef Van Boven <stefvboven2020@outlook.com>
2023-05-31 02:00:59 +0200

    added final content to my digital agency page writed an javascript file when you click on a picture the picture becomes biger and with the cloe button you can close it and i started writing content to my HAWK page

--- docs/js/image-collection.js
+++ docs/js/image-collection.js
@@ -1,19 +1,33 @@
-function addImageZoom() {
-    const imageContainers = document.querySelectorAll('.image-collection__container-image');
-  
-    imageContainers.forEach((container) => {
-      container.addEventListener('mouseover', () => {
-        container.style.transform = 'scale(2.3)';
-        container.style.zIndex = '1';
-      });
-  
-      container.addEventListener('mouseout', () => {
-        container.style.transform = 'scale(1)';
-        container.style.zIndex = '0';
-      });
+function addImageClickHandler() {
+  var images = document.querySelectorAll('.image-collection__container-image img');
+  var overlay = document.querySelector('.image-overlay');
+  var overlayImage = overlay.querySelector('img');
+  var closeButton = overlay.querySelector('.image-overlay__close');
+
+  images.forEach(function(image) {
+    image.addEventListener('click', function() {
+      var originalWidth = this.offsetWidth;
+      var originalHeight = this.offsetHeight;
+
+      this.classList.add('image-collection__container-image--enlarged');
+      this.style.width = originalWidth + 'px';
+      this.style.height = originalHeight + 'px';
+
+      overlayImage.src = this.src;
+      overlayImage.alt = this.alt;
+
+      overlay.style.display = 'flex';
     });
-  }
-  
-  // Call the function to add image zoom functionality
-  addImageZoom();
-     
+  });
+
+  closeButton.addEventListener('click', function() {
+    var enlargedImage = document.querySelector('.image-collection__container-image--enlarged');
+    enlargedImage.style.width = '';
+    enlargedImage.style.height = '';
+    enlargedImage.classList.remove('image-collection__container-image--enlarged');
+
+    overlay.style.display = 'none';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', addImageClickHandler);
