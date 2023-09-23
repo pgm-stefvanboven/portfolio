b1711c02efc8015dd286bd63505d6f37ce620fda
Stef Van Boven <stefvboven2020@outlook.com>
2023-09-23 18:11:39 +0200

    added the detailpages for the two projects and added the content on the pages

--- docs/js/image-collection.js
+++ docs/js/image-collection.js
@@ -1,13 +1,13 @@
 function addImageClickHandler() {
-  var images = document.querySelectorAll('.image-collection__container-image img');
-  var overlay = document.querySelector('.image-overlay');
-  var overlayImage = overlay.querySelector('img');
-  var closeButton = overlay.querySelector('.image-overlay__close');
+  const images = document.querySelectorAll('.image-collection__container-image img');
+  const overlay = document.querySelector('.image-overlay');
+  const overlayImage = overlay.querySelector('img');
+  const closeButton = overlay.querySelector('.image-overlay__close');
 
   images.forEach(function(image) {
     image.addEventListener('click', function() {
-      var originalWidth = this.offsetWidth;
-      var originalHeight = this.offsetHeight;
+      const originalWidth = this.offsetWidth;
+      const originalHeight = this.offsetHeight;
 
       this.classList.add('image-collection__container-image--enlarged');
       this.style.width = originalWidth + 'px';
@@ -21,7 +21,7 @@ function addImageClickHandler() {
   });
 
   closeButton.addEventListener('click', function() {
-    var enlargedImage = document.querySelector('.image-collection__container-image--enlarged');
+    const enlargedImage = document.querySelector('.image-collection__container-image--enlarged');
     enlargedImage.style.width = '';
     enlargedImage.style.height = '';
     enlargedImage.classList.remove('image-collection__container-image--enlarged');
